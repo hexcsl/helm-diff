d2ae6e312aa7cb56447de05f1f6bb76c863e25cc
hexcsl <hex@hexcsl.com>
2023-07-04 01:18:36 +0300

    mod release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,6 +6,17 @@ if [ "$1" = "" ]; then
   echo usage: "$0 VERSION"
 fi
 
-git tag $1
-git push origin $1
-gh release create $1 --draft --generate-notes --title "$1" release/*.tgz
+# git tag $1
+# git push origin $1
+# gh release create $1 --draft --generate-notes --title "$1" release/*.tgz
+
+user=hexcsl
+repo=helm-diff
+tag=$1
+commit=$2
+
+bin/github-release release -u $user -r $repo -t $tag -c $commit -n $tag
+
+for f in $(ls release); do
+  bin/github-release upload -u $user -r $repo -t $tag -n $f -f release/$f
+done
